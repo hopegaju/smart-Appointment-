dc61855f32aa3723c669eee6c101d4dd78906b38
Hope <hopegajupr@gmail.com>
2025-11-24 11:35:18 +0200

    Initialize Patient Service with basic Spring Boot setup

---
+++ patient-service/src/main/java/com/example/patient_service/PatientServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.patient_service;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+
+}
