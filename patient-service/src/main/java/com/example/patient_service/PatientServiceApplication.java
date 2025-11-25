05a2f97bdcf0026ee1c8237568b0ee3fcd2d78b6
Hope <hopegajupr@gmail.com>
2025-11-25 15:15:21 +0200

    Update PatientServiceApplication with configuration changes

--- patient-service/src/main/java/com/example/patient_service/PatientServiceApplication.java
+++ patient-service/src/main/java/com/example/patient_service/PatientServiceApplication.java
@@ -1,24 +1,14 @@
 package com.example.patient_service;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientServiceApplication.class, args);
+
+        SpringApplication.run(PatientServiceApplication.class, args);
 	}
 
 }
